d0a62c278191456e6aac27d2527628cff4055839
Mishel <mishtito19@gmail.com>
2023-05-03 13:37:52 -0400

    Add project files.

---
+++ circle/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Program to calculates circle");
+//pi*radio*radio
+var radio = 0d;
+var result = 0d;
+Console.WriteLine("Enter the value of radio");
+radio=Convert.ToDouble(Console.ReadLine());
+result= Math.Round((Math.PI*Math.Pow(2, radio)),2);
+Console.WriteLine(result);
